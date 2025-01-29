f96f16c32e3d31faacaea6e112e541ee64c7a3f1
Mauricio Murua <murua5@hotmail.com>
2025-01-29 11:05:51 -0300

    Initial commit on development branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },  // Ruta predeterminada
+  { path: '**', redirectTo: '' }  // Redirige cualquier ruta desconocida a la home
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
